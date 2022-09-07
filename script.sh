bbfd4db5a3bcc2e5c9d7232e437e4f165556240f
Kishor Shrestha <kishorshrestha625@gmail.com>
2022-09-07 15:37:22 +0545

    updated script

--- script.sh
+++ script.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-#random string of fixed length
+
+set -e;
+
+# random string of fixed length
 function get_random_string() {
   	head -3 /dev/urandom | tr -cd '[:alnum:]' | cut -c -$1;
 }
 
 function iterate_files (){
 		for file in *;do
-			random_string=$(get_random_string $1);
+		# store random string in to var	
+			random_string=$(get_random_string $2);
+		# get file ext
 			filename=$(basename $file)
 			fileext=$(echo "${filename##*.}")
-			
-			if [ "$fileext" != "sh" ];then
+		#rename file with ext
+		# $1 is the ext
+		# #2 is the length of string
+			if [ -z $1 ];then
+				echo "Renaming all files...";
 			 	mv $file "$random_string.$fileext";
+			else
+				echo "Renaming all $2 $1 files...";
+			 	mv $file "$random_string.$1";
 			fi
 		done;
 }
-set -e;
 
-if [ -z $1 ];then
-	iterate_files 5
+if [ -z $2 ];then
+	iterate_files "" 5
 else
- 	iterate_files $1;
+ 	iterate_files $1 $2;
 fi
 	
